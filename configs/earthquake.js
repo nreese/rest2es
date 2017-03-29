cf3970c3a73c137b12544d4e42b047f64fddc404
nreese <reese.nathan@gmail.com>
2017-03-29 11:10:19 -0600

    update earthquake config to better handle present day

--- configs/earthquake.js
+++ configs/earthquake.js
@@ -8,10 +8,17 @@ exports.config = {
       //https://earthquake.usgs.gov/fdsnws/event/1/
       let url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson'
       if (prevContext && prevContext.time) {
-        const prevDay = new Date(prevContext.time.min.substring(0,10));
-        const startDay = new Date(prevDay.getTime() + (24 * 60 * 60 * 1000));
-        const endDay = new Date(prevDay.getTime() + (2 * 24 * 60 * 60 * 1000));
-        url += `&starttime=${startDay.toISOString().substring(0,10)}&endtime=${endDay.toISOString().substring(0,10)}`;
+        const now = new Date();
+        if (prevContext.time.min.substring(0,10) === now.toISOString().substring(0,10)) {
+          const prevDay = new Date(prevContext.time.max);
+          const startDay = new Date(prevDay.getTime() + 1000);
+          url += `&starttime=${startDay.toISOString()}`;
+        } else {
+          const prevDay = new Date(prevContext.time.min.substring(0,10));
+          const startDay = new Date(prevDay.getTime() + (24 * 60 * 60 * 1000));
+          const endDay = new Date(prevDay.getTime() + (2 * 24 * 60 * 60 * 1000));
+          url += `&starttime=${startDay.toISOString().substring(0,10)}&endtime=${endDay.toISOString().substring(0,10)}`;
+        }
       } else {
         url += '&starttime=2013-01-01&endtime=2013-01-02';
       }
